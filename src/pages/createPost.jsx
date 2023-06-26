2d7ade158e15ffe9dbec7ca88e38194d8431deb3
Abhishek <anand2000ys@gmail.com>
2023-06-26 18:51:24 +0530

    utils page added

--- src/pages/createPost.jsx
+++ src/pages/createPost.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Preview from "../assets/preview.png";
+import { getRandomPrompt } from "../utils";
+import formField from "../components/Formfield/formField";
+import loader from "../components/Loader/loader";
 
 function createPost() {
   return <div>create-posts</div>;
