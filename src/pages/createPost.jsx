babc690293d59b04fe899045cefbbb9913641b9b
Abhishek <anand2000ys@gmail.com>
2023-06-27 12:38:05 +0530

    create post page design ready

--- src/pages/createPost.jsx
+++ src/pages/createPost.jsx
@@ -1,12 +1,41 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Form, useNavigate } from "react-router-dom";
 import Preview from "../assets/preview.png";
 import { getRandomPrompt } from "../utils";
-import formField from "../components/Formfield/formField";
+import FormField from "../components/Formfield/formField";
 import loader from "../components/Loader/loader";
 
 function createPost() {
-  return <div>create-posts</div>;
+  const navigate = useNavigate();
+  const [form, setForm] = React.useState({
+    name: "",
+    prompt: "",
+    photo: "",
+  });
+  const [generatingImg, setGeneratingImg] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  const handleSubmit = () => {};
+
+  return (
+    <div>
+      <section className="max-w-7xl mx-auto">
+        <div>
+          <h1 className="font-extrabold text-[#222328] text-[32px]">
+            Create a Post
+          </h1>
+          <p className="mt-2 text-[#666e75] text-[16px] max-w [500px]">
+            Create imaginative and visually stunning images throught DALL-E AI
+            and share them with the community
+          </p>
+        </div>
+        <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+          <div className="flex flec-col gap-5">
+            <FormField />
+          </div>
+        </form>
+      </section>
+    </div>
+  );
 }
 
 export default createPost;
