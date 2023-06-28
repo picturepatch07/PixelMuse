79c8d0d6262b55921e3e646b57d0a0be1c696757
Abhishek <anand2000ys@gmail.com>
2023-06-28 21:52:42 +0530

    api response added to create-post

--- src/pages/createPost.jsx
+++ src/pages/createPost.jsx
@@ -17,7 +17,27 @@ function createPost() {
 
   const [loading, setLoading] = React.useState(false);
 
-  const generateImage = () => {};
+  const generateImage = async () => {
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch("http://localhost:5000/api/dalle", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ prompt: form.prompt }),
+        });
+
+        const data = await response.json();
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+      } catch (err) {
+        alert(err);
+      } finally {
+        setGeneratingImg(flase);
+      }
+    }
+  };
 
   const handleSubmit = () => {};
 
